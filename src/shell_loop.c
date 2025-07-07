cfd7a2e7f1369f6bd6cf9831a8baf557d93b5623
Nicolás Maturana Chandia <nimatura@cbr15s5.42barcelona.com>
2025-07-07 21:59:47 +0200

    dev: revert to factorized code + g_state changees

--- src/shell_loop.c
+++ src/shell_loop.c
@@ -3,15 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   shell_loop.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/07 16:43:30 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/07/07 20:11:05 by aybelhaj         ###   ########.fr       */
+/*   Created: 2025/07/06 20:15:37 by nimatura          #+#    #+#             */
+/*   Updated: 2025/07/07 21:59:41 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// NOTE: Por qué caralho el g_in_input_phase? lo usa el readline?
+// En ese caso, capaz que no hay que pasarla por referencia
+// returns 1 in case of err, otherwise 0
+static int	readline_wrapper(char **line, t_shell *shell)
+{
+	*line = readline(PROMPT);
+	g_state.in_input = 0;
+	if (!line)
+	{
+		ft_putstr_fd("exit", STDOUT_FILENO);
+		free_array(shell->env);
+		return (1);
+	}
+	return (0);
+}
+
+// returns 1 in case of err, otherwise 0
+static int	tokenize_and_check_wrp(char **line, t_shell *shell)
+{
+	if (*line != NULL && **line != '\0')
+	{
+		add_history(*line);
+		shell->tokens = tokenize_line(*line);
+		if (!syntax_check(shell->tokens))
+		{
+			shell->cmd = parse_tokens(shell, shell->tokens);
+			if (shell->cmd)
+				return (0);
+		}
+	}
+	return (1);
+}
+
+static void	reset_cmd_line(char **line, t_shell *shell)
+{
+	free(line);
+	free_tokens(shell->tokens);
+	free_cmd_list(shell->cmd);  // Libera TODA la lista de comandos
+	shell->tokens = NULL;
+	shell->cmd = NULL;
+}
+
+// TODO: proteger dup
 int	shell_loop(t_shell *shell)
 {
 	char	*line;
@@ -21,35 +64,20 @@ int	shell_loop(t_shell *shell)
 	while (1)
 	{
 		g_state.in_input = 1;
-		line = readline(PROMPT);
-		g_state.in_input = 0;
-		if (!line)
-		{
-			ft_putstr_fd("exit\n", STDOUT_FILENO);
-			free_array(shell->env);
+		if (0 != readline_wrapper(&line, shell))
 			break ;
-		}
 		if (*line)
 		{
-			add_history(line);
-			shell->tokens = tokenize_line(line);
-			if (!syntax_check(shell->tokens))
-			{
-				shell->cmd = parse_tokens(shell, shell->tokens);
-				if (shell->cmd)
-					execute_cmd(shell, shell->cmd);
-			}
+			if (0 == tokenize_and_check_wrp(&line, shell) && shell->cmd)
+				execute_cmd(shell, shell->cmd);
 			else
 			{
 				ft_putstr_fd("minishell: syntax error\n", STDERR_FILENO);
 				shell->last_status = 2;
 			}
 		}
-		free(line);
-		free_tokens(shell->tokens);
-		free_cmd_list(shell->cmd);
-		shell->cmd = NULL;
-		reset_std_fds(std_backup);
+		reset_cmd_line(&line, shell);
+		reset_std_fds(std_backup); //Restaurar FDs estándar después de cada comando
 	}
 	close(std_backup[0]);
 	close(std_backup[1]);
