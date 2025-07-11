d7de709996a91d7098793e5c11192b4caa6dcb46
Nicolás Maturana Chandia <nimatura@car8s1.42barcelona.com>
2025-07-11 19:01:52 +0200

    dev: ft_lstremove_and_link for builtin_unset

--- libft/src/ft_lstremove_and_link.c
+++ libft/src/ft_lstremove_and_link.c
@@ -6,18 +6,20 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:54:31 by nimatura          #+#    #+#             */
-/*   Updated: 2025/07/11 18:58:04 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/11 19:01:50 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 
+// Removes a [node] from the [head] lst. If the node is not found, the function\
+// will not do anything
 void	ft_lstremove_and_link(t_list **head, t_list *node)
 {
 	t_list *tmp;
 	t_list *next;
 
-	if (*head == NULL)
+	if (*head == NULL || node == NULL)
 		return ;
 	if (*head == node)
 	{
@@ -32,6 +34,8 @@ void	ft_lstremove_and_link(t_list **head, t_list *node)
 			tmp = tmp->next;
 		if (tmp->next != node)
 			return ;
-
+		next = (tmp->next)->next;
+		ft_lstdelone(tmp->next, &free);
+		tmp->next = next;
 	}
 }
