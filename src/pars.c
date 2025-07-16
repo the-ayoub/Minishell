33c3a10e7f71e5ffbc52bfa9c01db8ed57f78471
Nicolás Maturana Chandia <nimatura@cbr10s2.42barcelona.com>
2025-07-16 20:28:21 +0200

    fix: syntax err at every input bug: cmds aren't executed

--- src/pars.c
+++ src/pars.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:42:45 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/07/16 20:15:55 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:27:51 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -204,6 +204,7 @@ static t_cmd	*create_new_command(void)
 	return (new);
 }
 
+// returns 1 in success, 0 for err
 static int	parse_pipe(t_token **current, t_cmd **cmd, t_cmd **ptr)
 {
 	if (NULL == *cmd)
@@ -247,13 +248,11 @@ int	parse_tokens(t_shell *shell, t_token *tokens, t_cmd **ptr)
 	current = tokens;
 	while (current != NULL)
 	{
-		if (current->type == TOKEN_PIPE && \
-			parse_pipe(&current, &current_cmd, ptr))
-			continue ;
-		else
+		if (current->type == TOKEN_PIPE)
 		{
-			printf("%p	head\n", ptr);
-			return (0);
+			if (parse_pipe(&current, &current_cmd, ptr) == 0)
+				return (0);
+			continue ;
 		}
 		if (NULL == current_cmd)
 		{
