df23986b5cd4314259acf9f67052aba01b44dbc5
Nicolás Maturana Chandia <nimatura@cbr10s2.42barcelona.com>
2025-07-16 20:16:09 +0200

    parse_tokens.c:253: else triggered at second iteration forcing syntax err

--- src/pars.c
+++ src/pars.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:42:45 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/07/16 19:46:38 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:15:55 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,8 +82,6 @@ void	append_char(char **str, char c)
 }
 
 // WARNING: If 1 token only, there's no current->next->type
-
-// returns 1 in case of err
 int	syntax_check(t_token *tokens)
 {
 	t_token			*current;
@@ -253,18 +251,21 @@ int	parse_tokens(t_shell *shell, t_token *tokens, t_cmd **ptr)
 			parse_pipe(&current, &current_cmd, ptr))
 			continue ;
 		else
+		{
+			printf("%p	head\n", ptr);
 			return (0);
+		}
 		if (NULL == current_cmd)
 		{
 			current_cmd = create_new_command();
 			head = current_cmd;
 		}
-		if (is_token_word(current->type)) //if word
+		if (is_token_word(current->type))
 			add_argument(current_cmd, ft_strdup(current->value));
 		else if (current->type >= TOKEN_REDIR_IN \
-				&& current->type <= TOKEN_HEREDOC) // if redir
+				&& current->type <= TOKEN_HEREDOC)
 			parse_redir(&current, &current_cmd, ptr);
-		else	//err
+		else
 		{
 			ft_putstr_fd("minishell: unknown token type\n", STDERR_FILENO);
 			free_cmd_list(head);
