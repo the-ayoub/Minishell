63b6fd01483916ff1748a3d892d2ead4336a3228
Nicolás Maturana Chandia <nimatura@car11s1.42barcelona.com>
2025-07-18 19:27:34 +0200

    fix: leaks bug: wont expand 2 vars such as $PATH$USER

--- src/expansion.c
+++ src/expansion.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:39:44 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/07/18 19:25:57 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/18 19:27:14 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ char	*assemble_expansion(char *token_value, t_list *env_value, char *var)
 		end = ft_strjoin(end, token_value);
 		free(prev);
 	}
-	free(var);
 	return (end);
 }
 
