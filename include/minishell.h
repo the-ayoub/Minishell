a78d3f5f8d437398cc6e8c58d425f5c1e961a4fe
Nicolás Maturana Chandia <nimatura@cbr2s6.42barcelona.com>
2025-07-21 23:37:29 +0200

    update: wrappers

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 19:46:54 by nimatura          #+#    #+#             */
-/*   Updated: 2025/07/21 22:39:39 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/21 23:32:01 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/inc/libft.h"
+# include "ms_structs.h"
 
 // === CONSTANTES ===
 # define PROMPT "minishell> "
@@ -38,72 +39,6 @@
 # define ERROR 1
 # define PATH_MAX 4096  // Tamaño razonable para la mayoría de sistemas
 
-// Estados para el análisis de comillas
-typedef enum e_parse_state
-{
-    STATE_GENERAL,
-    STATE_IN_SQUOTE,
-    STATE_IN_DQUOTE
-} t_parse_state;
-
-// === TIPOS DE TOKENS ===
-typedef enum e_token_type
-{
-	TOKEN_WORD,         // palabra (comando o argumento)
-	TOKEN_WORD_SQ,         // palabra (comando o argumento)
-	TOKEN_WORD_DQ,         // palabra (comando o argumento)
-	TOKEN_PIPE,         // |
-	TOKEN_REDIR_IN,     // <
-	TOKEN_REDIR_OUT,    // >
-	TOKEN_REDIR_APPEND, // >>
-	TOKEN_HEREDOC,      // <<
-}	t_token_type;
-
-// === ESTRUCTURA DE UN TOKEN ===
-typedef struct s_token
-{
-	t_token_type	type;
-	char		    *value;
-	struct s_token	*next;
-}	t_token;
-
-// === TIPOS DE REDIRECCIÓN ===
-typedef enum e_redir_type
-{
-	REDIR_IN,       // <
-	REDIR_OUT,      // >
-	REDIR_APPEND,   // >>
-	REDIR_HEREDOC   // <<
-}	t_redir_type;
-
-// === ESTRUCTURA DE REDIRECCIÓN ===
-typedef struct s_redir
-{
-	t_redir_type	type;
-	char		    *file;  // nombre del archivo o delimitador
-	struct s_redir	*next;
-}	t_redir;
-
-// === ESTRUCTURA DE UN COMANDO ===
-typedef struct s_cmd
-{
-	char			**argv;     // argumentos del comando
-	t_redir			*redirs;    // lista de redirecciones
-	struct s_cmd	*next;  // siguiente comando (en un pipe)
-}	t_cmd;
-
-// === ESTRUCTURA DEL SHELL ===
-typedef struct s_shell
-{
-	struct termios	orig_termios;   // configuración original del terminal
-	t_list			*raw_env;
-	char			**env;              // copia del entorno
-	int				last_status;        // estado de salida del último comando
-	t_token			*tokens;            // tokens de la línea de comando actual
-	t_cmd			*cmd;               // comando actual en ejecución
-	int				interactive;        // 1 si el shell es interactivo
-	int			    in_input_phase;  // Nuevo: para manejo de señales
-}	t_shell;
 
 // === VARIABLES GLOBALES ===
 typedef struct s_global
@@ -194,6 +129,10 @@ int		builtin_env(t_shell *shell, char **argv);
 int		builtin_exit(t_shell *shell, char **argv);
 int		is_valid_identifier(char *str);
 int		is_valid_exit_arg(char *arg);
+
+// === WRAPPERS ===
+int	wrapper_dup(int *holder, int fd, t_shell *shell);
+int	wrapper_dup2(int oldfd, int newfd, t_shell *shell);
 
 // === UTILS ===
 void	error_msg(const char *msg);
