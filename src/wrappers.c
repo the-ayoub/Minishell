bbc1be9b9fb2b4c6f24ee8222d90996cca51a668
Nicolás Maturana Chandia <nimatura@cbr2s6.42barcelona.com>
2025-07-22 00:13:23 +0200

    upd: typo

--- src/wrappers.c
+++ src/wrappers.c
@@ -6,7 +6,7 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 23:03:22 by nimatura          #+#    #+#             */
-/*   Updated: 2025/07/21 23:30:01 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/22 00:13:14 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	wrapper_dup2(int oldfd, int newfd, t_shell *shell)
 {
 	if (dup2(oldfd, newfd) == -1)
 	{
-		perror("execute_cmd: dup2 failed");
+		perror("dup2 failed");
 		shell->last_status = 1;
 		return (FALSE);
 	}
@@ -28,7 +28,7 @@ int	wrapper_dup(int *holder, int fd, t_shell *shell)
 	*holder = dup(fd);
 	if (*holder == -1)
 	{
-		perror("execute_cmd: dup2 failed");
+		perror("dup failed");
 		shell->last_status = 1;
 		return (FALSE);
 	}
