63bb7c178a39b495320d3b3881f4cd953ca29f2d
Nicolás Maturana Chandia <nimatura@cbr1s6.42barcelona.com>
2025-07-23 18:36:48 +0200

    dev: cleanup_assamble_expansion

--- src/expansion.c
+++ src/expansion.c
@@ -6,18 +6,48 @@
 /*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:39:44 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/07/22 21:58:16 by nimatura         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:36:27 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static int	aux_upd_data(t_expand *data, char *match, char *var_name, char *env_var)
+void	init_expand(t_expand *data)
+{
+	data->token_str = NULL;
+	data->to_expand = NULL;
+	data->matching_env = NULL;
+	data->var_name = NULL;
+}
+
+int	aux_upd_data(t_expand *data, char *match, char *var_name, char *env_var)
 {
 	data->matching_env = env_var;
 	data->to_expand = match;
 	data->var_name = var_name;
 	return (1);
+}
+
+char	*assemble_expansion(char *token_value, t_expand *data)
+{
+	size_t	i;
+	char	*new;
+	size_t	len;
+
+	i = data->to_expand - token_value;
+	new = ft_substr(token_value, 0, i);
+	if (new == NULL)
+		return (NULL);
+	data->matching_env = ft_strchr(data->matching_env, '=');
+	if (data->matching_env == NULL)
+		perror("cant assamble expansion");
+	if (wrapper_strjoin(&new, ++data->matching_env) == FALSE)
+		perror("cant assamble expansion");
+	len = ft_strlen(data->var_name) + 1;
+	data->to_expand += len;
+	if (wrapper_strjoin(&new, data->to_expand) == FALSE)
+		perror("cant assamble expansion");
+	return (new);
 }
 
 // Retrieves the $str from the token.value, then iterates over env_lst
@@ -32,7 +62,6 @@ static int	is_expandable(t_list *env_lst, t_token token, t_expand *data)
 	if (token.type != TOKEN_WORD_DQ && token.type != TOKEN_WORD)
 		return (1);
 	iter = token.value;
-	var_name = NULL;
 	match = NULL;
 	while (match == NULL && *iter != '\0')
 	{
@@ -56,50 +85,38 @@ static int	is_expandable(t_list *env_lst, t_token token, t_expand *data)
 	return (0);
 }
 
-char	*assemble_expansion(char *token_value, t_expand *data)
-{
-	size_t	i;
-	char	*new;
-	char	*tmp;
-	char	*result;
-	size_t	len;
-
-	i = data->to_expand - token_value;
-	new = ft_substr(token_value, 0, i);
-	if (new == NULL)
-		return (NULL);
-	data->matching_env = ft_strchr(data->matching_env, '=');
-	if (data->matching_env == NULL)
-		perror("cant assamble expansion");
-	tmp = ft_strjoin(new, ++data->matching_env);
-	if (tmp == NULL)
-		perror("cant assamble expansion");
-	free(new);
-	new = tmp;
-	len = ft_strlen(data->var_name) + 1;
-	data->to_expand += len;
-	tmp = ft_substr(data->to_expand, 0, ft_strlen(data->to_expand));
-	if (NULL == new)
-		perror("cant assamble expansion");
-	result = ft_strjoin(new, tmp);
-	free(new);
-	free(tmp);
-	return (result);
-}
-
-void	init_expand(t_expand *ptr)
-{
-	ptr->token_str = NULL;
-	ptr->to_expand = NULL;
-	ptr->matching_env = NULL;
-	ptr->var_name = NULL;
-}
+// int	expand_variables(t_shell *shell, t_token *token)
+// {
+// 	t_expand	data;
+// 	char		*tmp;
+// 	
+// 	init_expand(&data);
+// 	while (NULL != token)
+// 	{
+// 		data.token_str = token->value;
+// 		if (0 == is_expandable(shell->raw_env, *token, &data)) // ok
+// 		{
+// 			tmp = assemble_expansion(token->value, &data);
+// 			if (NULL == tmp)
+// 			{
+// 				token = token->next;
+// 				continue ;
+// 			}
+// 			free(token->value);
+// 			token->value = tmp;
+// 		}
+// 		free(data.var_name);
+// 		token = token->next;
+// 	}
+// 	free(data.var_name);
+// 	return (0);
+// }
 
 int	expand_variables(t_shell *shell, t_token *head)
 {
 	t_expand	data;
-	t_token	*token;
-	char	*tmp;
+	t_token		*token;
+	char		*tmp;
 	
 	init_expand(&data);
 	token = head;
@@ -122,5 +139,6 @@ int	expand_variables(t_shell *shell, t_token *head)
 		free(token->value);
 		token->value = tmp;
 	}
+	free(data.var_name);
 	return (0);
 }
